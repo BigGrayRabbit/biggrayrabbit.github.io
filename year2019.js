c8be90da81f3a1667083ff327acc88e297868577
sagittaria <shuyang_x@hotmail.com>
2019-08-06 00:18:03 +0800

    Then of thy beauty do I question make

---
+++ year2019.js
@@ -0,0 +1,27 @@
+ var year2019 = 
+      {
+        season3: [
+         {
+            name: 'lesson',
+            color: '#CC92BC',
+            target: 3*4, // 3*4，共3集，每集4小节
+            steps: [
+              { id: 0, timestamp: '2019-07-21 20:10', content: 'u3e5-4', point: 1 },
+            ],
+          }, {
+            name: 'expounding',
+            color: '#13ce66',
+            target: 2400,
+            steps: [
+              { id: 0, timestamp: '2019-05-18 09:00', content: '第1.5-写完听完', period: 120 },
+            ],
+          }, {
+            name: 'aptitude',
+            color: '#409EFF',
+            target: 6000,
+            steps: [
+              { id: 0, timestamp: '2019-05-18 14:30', content: '18年省考-数学不做', period: 90 },
+            ],
+          },
+        ]
+      }
