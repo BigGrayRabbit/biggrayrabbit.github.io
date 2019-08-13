23539a62ffb63a3208cf9db1b4d70817ba6fa322
sagittaria <shuyang_x@hotmail.com>
2019-08-13 21:36:08 +0800

    hmmm

--- year2019.js
+++ year2019.js
@@ -1,26 +1,19 @@
- var year2019 = 
+ var year2019 =
       {
         season3: [
-         {
-            name: 'lesson',
-            color: '#CC92BC',
-            target: 3*4, // 3*4，共3集，每集4小节
-            steps: [
-              { id: 0, timestamp: '2019-07-21 20:10', content: 'u3e5-4', point: 1 },
-            ],
-          }, {
-            name: 'expounding',
+          {
+            name: 'beyer',
             color: '#13ce66',
-            target: 2400,
+            target: 30,
             steps: [
-              { id: 0, timestamp: '2019-05-18 09:00', content: '第1.5-写完听完', period: 120 },
+              { id: 0, timestamp: '2019-08-13 21:00', content: '#10', point: 1 },
             ],
           }, {
-            name: 'aptitude',
+            name: 'song',
             color: '#409EFF',
-            target: 6000,
+            target: 9,
             steps: [
-              { id: 0, timestamp: '2019-05-18 14:30', content: '18年省考-数学不做', period: 90 },
+              { id: 0, timestamp: '2019-08-11 21:30', content: '音程一、二', point: 2 },
             ],
           },
         ]
