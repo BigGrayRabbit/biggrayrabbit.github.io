67e14beaca846631454f2e5c38e3b12d19673d8c
sagittaria <shuyang_x@hotmail.com>
2019-08-20 21:33:28 +0800

    hmm

--- year2019.js
+++ year2019.js
@@ -14,6 +14,8 @@
             target: 9,
             steps: [
               { id: 0, timestamp: '2019-08-11 21:30', content: '音程一、二', point: 2 },
+              { id: 1, timestamp: '2019-08-18 20:45', content: '大音阶，小音阶', point: 2 },
+              { id: 2, timestamp: '2019-08-20 21:30', content: 'C大调和弦-正三和弦名称及弹奏', point: 2 },
             ],
           },
         ]
