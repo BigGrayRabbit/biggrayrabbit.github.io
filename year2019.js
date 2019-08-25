bed52f93ab444b5023b68125b4487d78b423e352
sagittaria <shuyang_x@hotmail.com>
2019-08-25 21:49:00 +0800

    hm

--- year2019.js
+++ year2019.js
@@ -6,7 +6,7 @@
             color: '#13ce66',
             target: 30,
             steps: [
-              { id: 0, timestamp: '2019-08-13 21:00', content: '#10', point: 1 },
+            { id: 0, timestamp: '2019-08-25 17:00', content: '#12, #13(实际是从第12条开始的)', point: 13 },
             ],
           }, {
             name: 'song',
@@ -16,6 +16,7 @@
               { id: 0, timestamp: '2019-08-11 21:30', content: '音程一、二', point: 2 },
               { id: 1, timestamp: '2019-08-18 20:45', content: '大音阶，小音阶', point: 2 },
               { id: 2, timestamp: '2019-08-20 21:30', content: 'C大调和弦-正三和弦名称及弹奏', point: 2 },
+              { id: 3, timestamp: '2019-08-24 20:20', content: 'C大调属七和弦', point: 1 },
             ],
           },
         ]
