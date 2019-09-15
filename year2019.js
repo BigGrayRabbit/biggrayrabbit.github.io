0570fd40e3e9df661281c1376ff3cba0df093ecd
sagittaria <shuyang_x@hotmail.com>
2019-09-15 19:45:11 +0800

    h

--- year2019.js
+++ year2019.js
@@ -7,6 +7,8 @@
             target: 30,
             steps: [
             { id: 0, timestamp: '2019-08-25 17:00', content: '#12, #13(实际是从第12条开始的)', point: 13 },
+            { id: 1, timestamp: '2019-09-01 16:00', content: '#14', point: 1 },
+            { id: 2, timestamp: '2019-09-15 15:00', content: '#15', point: 1 },
             ],
           }, {
             name: 'song',
@@ -17,6 +19,7 @@
               { id: 1, timestamp: '2019-08-18 20:45', content: '大音阶，小音阶', point: 2 },
               { id: 2, timestamp: '2019-08-20 21:30', content: 'C大调和弦-正三和弦名称及弹奏', point: 2 },
               { id: 3, timestamp: '2019-08-24 20:20', content: 'C大调属七和弦', point: 1 },
+              { id: 4, timestamp: '2019-09-15 20:00', content: '低音和弦与转位，踏板', point: 1 },
             ],
           },
         ]
